37079140b6f3ba703853a7a9c82782bf4700c21d
Abhimanyu9988 <32825278+Abhimanyu9988@users.noreply.github.com>
2022-12-28 13:49:02 -0600

    Update appdynamics_variables.tf

--- appdynamics_variables.tf
+++ appdynamics_variables.tf
@@ -2,7 +2,7 @@
 variable "APPDYNAMICS_CONTROLLER_URL" {
   type        = string
   description = "The controller host name without http(s) or port number."
-  default = "https://controllerces.saas.appdynamics.com"
+  default = "https://XXXXX.saas.appdynamics.com"
 
   # validation {
   #   condition     = length(var.APPDYNAMICS_CONTROLLER_URL) > 4 && substr(var.APPDYNAMICS_CONTROLLER_URL, 0, 4) != "http"
@@ -13,7 +13,7 @@ variable "APPDYNAMICS_CONTROLLER_URL" {
 variable "APPDYNAMICS_AGENT_ACCOUNT_NAME" {
   type        = string
   description = "The AppDynamics controller account name."
-  default = "controllerces"
+  default = "XXXXX"
 
   validation {
     condition     = length(var.APPDYNAMICS_AGENT_ACCOUNT_NAME) > 0
@@ -23,7 +23,7 @@ variable "APPDYNAMICS_AGENT_ACCOUNT_NAME" {
 
 variable "APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY" { # sensitive value
   type = string
-  default = "bm9xa03c6d2p"
+  default = "XXXXXX"
 
   validation {
     condition     = length(var.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY) > 0
